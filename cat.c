1d6a2fc91e497639d7886e82fa3dddb88f2876f9
Kami <ljgb.blbgkd@gmail.com>
2020-05-08 00:20:48 +0900

    Add support for multiple files

--- cat.c
+++ cat.c
@@ -6,26 +6,28 @@ int main(int argc, char *argv[]) {
     FILE *fp;
 
     char *fname = argv[1];
-    fp = fopen(fname, "r");
     int chr;
     int buf = 256;
     char str[buf];
-    while (fname == NULL) {
+    argv++;
+    while (*argv == NULL) {
         scanf("%s", str);
         if (strcmp(str, "\\D") == 0) {
             return 0;
         }
         printf("%s\n", str);
     }
-
-    if (fp == NULL) {
-        printf("cat: %s: No such file or directory\n", fname);
-        return -1;
+    for (; *argv != NULL; argv++) {
+        fname = *argv;
+        fp = fopen(fname, "r");
+        if (fp == NULL) {
+            printf("cat: %s: No such file or directory\n", fname);
+            return -1;
+        }
+        while ((fgets(str, buf, fp)) != NULL) {
+            printf("%s", str);
+        }
+        printf("\n");
+        fclose(fp);
     }
-
-    while ((fgets(str, buf, fp)) != NULL) {
-        printf("%s", str);
-    }
-    printf("\n");
-    fclose(fp);
 }
